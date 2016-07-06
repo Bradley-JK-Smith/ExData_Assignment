3dc175a7afacdef134731ee222611622e9cbbaae
Bradley JK Smith <bradley.jk.smith@nab.com.au>
2016-07-06 17:51:34 +1000

    SECOND CUT
    Added plot 4
    Additional parameters

--- tmp.R
+++ tmp.R
@@ -10,10 +10,12 @@ x <- group_by(NEI, year) %>%
         summarise(pm25 = sum(Emissions)/1e6)
 
 plot(x,
-     las=1,
-     main='Total PM2.5 emissions in United States by year',
-     xlab='Year',
-     ylab='Total PM2.5 emissions (millions tons)'
+     las  = 1,
+     pch  = 19,
+     type = 'b',
+     main = 'Total PM2.5 emissions in United States by year',
+     xlab = 'Year',
+     ylab = 'Total PM2.5 emissions (millions tons)'
     )
 
 # Plot 2
@@ -23,10 +25,12 @@ x <- group_by(NEI, year) %>%
         summarise(pm25 = sum(Emissions)/1e3)
 
 plot(x,
-     las=1,
-     main='Total PM2.5 emissions in Baltimore City by year',
-     xlab='Year',
-     ylab='Total PM2.5 emissions (thousands tons)'
+     las  = 1,
+     pch  = 19,
+     type = 'b',
+     main = 'Total PM2.5 emissions in Baltimore City by year',
+     xlab = 'Year',
+     ylab = 'Total PM2.5 emissions (thousands tons)'
     )
 
 # Plot 3
@@ -39,11 +43,30 @@ x <- group_by(NEI, year, type) %>%
 # Facets helps split out the data
 # DON'T use linear fit because the question is which
 # type has increase. 
-qplot(year, pm25, data=x,
+qplot(year,
+      pm25,
+      data   = x,
       facets = . ~ type,
-      color=type,
-      geom=c("point", "line")
+      geom   = c("point", "line"),
+      main   = 'Total PM2.5 Emissions in Baltimore City by year',
+      ylab   = 'Total PM2.5 (thousand tons)'
       )
 
-,
-      method="lm")
+# Plot 4
+
+idx <- grepl('combustion', SCC$SCC.Level.Four, ignore.case=T)
+idy <- grepl('coal', SCC$SCC.Level.Four, ignore.case=T)
+unique(SCC[idx&idy,'SCC.Level.Four'])
+
+x <- group_by(NEI[idx&idy,], year) %>%
+        summarise(pm25 = sum(Emissions)/1e3)
+
+plot(x,
+     las  = 1,
+     pch  = 19,
+     type = 'b',
+     main = 'Total PM2.5 emissions in United States from coal combustion sources by year',
+     xlab = 'Year',
+     ylab = 'Total PM2.5 emissions (thousands tons)'
+    )
+  
